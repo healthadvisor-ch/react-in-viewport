277857333c544671f7f86ce2c350eaf0a85163a1
Michal Moczulski <ma.moczulski@gmail.com>
2022-06-01 16:31:38 +0200

    Add dist

---
+++ dist/es/lib/useInViewport.js
@@ -0,0 +1,143 @@
+"use strict";
+
+exports.__esModule = true;
+exports.default = void 0;
+
+var _react = require("react");
+
+var _reactDom = require("react-dom");
+
+var _constants = require("./constants");
+
+function _extends() { _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
+
+var useInViewport = function useInViewport(target, options, config, props) {
+  if (options === void 0) {
+    options = _constants.defaultOptions;
+  }
+
+  if (config === void 0) {
+    config = _constants.defaultConfig;
+  }
+
+  if (props === void 0) {
+    props = _constants.defaultProps;
+  }
+
+  var {
+    onEnterViewport,
+    onLeaveViewport
+  } = props;
+  var [, forceUpdate] = (0, _react.useState)();
+  var observer = (0, _react.useRef)();
+  var inViewportRef = (0, _react.useRef)(false);
+  var intersected = (0, _react.useRef)(false);
+  var enterCountRef = (0, _react.useRef)(0);
+  var leaveCountRef = (0, _react.useRef)(0);
+
+  function startObserver(_ref) {
+    var {
+      observerRef
+    } = _ref;
+    var targetRef = target.current;
+
+    if (targetRef) {
+      var node = (0, _reactDom.findDOMNode)(targetRef);
+
+      if (node) {
+        observerRef == null ? void 0 : observerRef.observe(node);
+      }
+    }
+  }
+
+  function stopObserver(_ref2) {
+    var {
+      observerRef
+    } = _ref2;
+    var targetRef = target.current;
+
+    if (targetRef) {
+      var node = (0, _reactDom.findDOMNode)(targetRef);
+
+      if (node) {
+        observerRef == null ? void 0 : observerRef.unobserve(node);
+      }
+    }
+
+    observerRef == null ? void 0 : observerRef.disconnect();
+    observer.current = null;
+  }
+
+  function handleIntersection(entries) {
+    var entry = entries[0] || {};
+    var {
+      isIntersecting,
+      intersectionRatio
+    } = entry;
+    var isInViewport = typeof isIntersecting !== 'undefined' ? isIntersecting : intersectionRatio > 0; // enter
+
+    if (!intersected.current && isInViewport) {
+      intersected.current = true;
+      onEnterViewport == null ? void 0 : onEnterViewport();
+      enterCountRef.current += 1;
+      inViewportRef.current = isInViewport;
+      forceUpdate(isInViewport);
+      return;
+    } // leave
+
+
+    if (intersected.current && !isInViewport) {
+      intersected.current = false;
+      onLeaveViewport == null ? void 0 : onLeaveViewport();
+
+      if (config.disconnectOnLeave && observer.current) {
+        // disconnect obsever on leave
+        observer.current.disconnect();
+      }
+
+      leaveCountRef.current += 1;
+      inViewportRef.current = isInViewport;
+      forceUpdate(isInViewport);
+    }
+  }
+
+  function initIntersectionObserver(_ref3) {
+    var {
+      observerRef
+    } = _ref3;
+
+    if (!observerRef) {
+      var root = typeof options.root === 'function' ? options.root() : options.root;
+      observer.current = new IntersectionObserver(handleIntersection, _extends({}, options, {
+        root
+      }));
+      return observer.current;
+    }
+
+    return observerRef;
+  }
+
+  (0, _react.useEffect)(() => {
+    var observerRef = observer.current; // https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API
+
+    observerRef = initIntersectionObserver({
+      observerRef
+    });
+    startObserver({
+      observerRef
+    });
+    return () => {
+      stopObserver({
+        observerRef
+      });
+    };
+  }, [target.current, options, config, onEnterViewport, onLeaveViewport]);
+  return {
+    inViewport: inViewportRef.current,
+    enterCount: enterCountRef.current,
+    leaveCount: leaveCountRef.current
+  };
+};
+
+var _default = useInViewport;
+exports.default = _default;
